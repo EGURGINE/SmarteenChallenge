a3999d4e4e572fd91fc147372015968275ef289d
EGURGINE <sdh210202@sdh.hs.kr>
2022-04-17 21:30:49 +0900

    청소기 업글 아이템

--- Assets/JunHo/Scripts/GameManager.cs
+++ Assets/JunHo/Scripts/GameManager.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; set; }
+
+    [Header("환경게이지")]
     [SerializeField] private Slider environmentalGauge;
     public float environmentalGaugeMax;
     public float environmentalGaugeCur;
 
+    [Header("플레이어 속성")]
+    private int hooverUp;
+    [SerializeField] private BoxCollider playerHoover;
     private void Awake()
     {
         Instance = this;
@@ -21,6 +26,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.F))
+        {
+            ItemManager(3);
+        }
         SliderManager();
     }
     public void ItemManager(int num)
@@ -34,6 +43,11 @@ public class GameManager : MonoBehaviour
                 GameObject.Find("Player").GetComponent<Player>().Speed += 5;
                 break;
             case 3:
+                if (hooverUp >= 5) return;
+
+                playerHoover.size =new Vector3(playerHoover.size.x, playerHoover.size.y, playerHoover.size.z+1);
+                playerHoover.center = new Vector3(playerHoover.center.x, playerHoover.center.y, playerHoover.center.z + 0.5f);
+                hooverUp++;
                 //공속 && 범위 증가
                 break;
             case 4:
